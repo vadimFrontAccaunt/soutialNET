294e495ed5a3f436c57afb842bdf6e847cd5d6a1
Вадим Скороход <vadim.official.page@gmail.com>
2022-02-04 15:13:18 +0200

    Updates

---
+++ precache-manifest.faa38d7def68e442f643c9a4613cc428.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ace5b03e0a34a6bdb125411ab6354b1",
+    "url": "/soutialNET/index.html"
+  },
+  {
+    "revision": "80c54ad0cd923d4fe3ed",
+    "url": "/soutialNET/static/css/2.44309f2b.chunk.css"
+  },
+  {
+    "revision": "f38b34e6fd7d091cb7e5",
+    "url": "/soutialNET/static/css/3.4e49c2be.chunk.css"
+  },
+  {
+    "revision": "4881540753b36e78ae11",
+    "url": "/soutialNET/static/css/main.0e6a4573.chunk.css"
+  },
+  {
+    "revision": "80c54ad0cd923d4fe3ed",
+    "url": "/soutialNET/static/js/2.17f6998b.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/soutialNET/static/js/2.17f6998b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f38b34e6fd7d091cb7e5",
+    "url": "/soutialNET/static/js/3.71ad014e.chunk.js"
+  },
+  {
+    "revision": "630038500fd5c45e9396",
+    "url": "/soutialNET/static/js/4.58316c6e.chunk.js"
+  },
+  {
+    "revision": "4881540753b36e78ae11",
+    "url": "/soutialNET/static/js/main.70271f87.chunk.js"
+  },
+  {
+    "revision": "bb41b40a1c6e5988539e",
+    "url": "/soutialNET/static/js/runtime-main.2f53e5f0.js"
+  },
+  {
+    "revision": "6c3daf6085e46eea1698990ad3578119",
+    "url": "/soutialNET/static/media/preloader.6c3daf60.svg"
+  },
+  {
+    "revision": "8b1dbb1f4e4468b9cc31ced9c63fcfe7",
+    "url": "/soutialNET/static/media/user.8b1dbb1f.png"
+  }
+]);
